681c78400ba6bd6c5badc74bd252080ed7948331
MistrKraus <petahousik@gmail.com>
2017-11-24 10:38:54 +0100

    Inicializacni commit

---
+++ src/java/MainController.java
@@ -0,0 +1,20 @@
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    public ComboBox rawWords;
+    public ComboBox cipherdWords;
+    public TextArea inputText;
+    public TextArea outputText;
+    public TextArea keysText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
